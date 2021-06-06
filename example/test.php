fbb3cad19536b3cccefeba9421bff8743b2cd8c1
Fathur Rohman <vatoer.ckplus@gmail.com>
2021-06-06 17:23:23 +0700

    compose

--- example/test.php
+++ example/test.php
@@ -1,8 +1,8 @@
 <?php 
 require dirname(__DIR__, 1) . '/vendor/autoload.php';
-use Kemlu\Oauth2\Client\Provider\KemluUser;
+//use Kemlu\Oauth2\Client\Provider\KemluUser;
 
-$a = ['id'=>1,'email'=>'abc.@dfd.com'];
-$user = new KemluUser($a);
-var_dump(get_declared_classes()); 
+//$a = ['id'=>1,'email'=>'abc.@dfd.com'];
+//$user = new KemluUser($a);
+print_r(get_declared_classes()); 
 //phpinfo();
